6a8d09a0fd08c9935d7363b3a44a009417968d5e
ahmedfouaddev <ahmed@eventkoi.com>
2025-08-16 02:51:00 +0300

    Update css

--- scripts/frontend/build/index.asset.php
+++ scripts/frontend/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-primitives'), 'version' => '7fc0c08fe63626cfa0e7');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-primitives'), 'version' => '8079bb7467e54d80be5d');
