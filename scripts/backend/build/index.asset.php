1bf9940880c943c82826638708fd40ec4fc7b89f
ahmedfouaddev <ahmed@eventkoi.com>
2025-09-22 11:44:04 +0300

    Remove licensing

--- scripts/backend/build/index.asset.php
+++ scripts/backend/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-primitives'), 'version' => 'd96ce8277d18f863b628');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-primitives'), 'version' => 'a53cc93ae34560d22970');
