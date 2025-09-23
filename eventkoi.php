c584c7e618b33538cb69048f9db8c390fced87c4
ahmedfouaddev <ahmed@eventkoi.com>
2025-09-23 14:01:25 +0300

    Update eventkoi.php

--- eventkoi.php
+++ eventkoi.php
@@ -8,7 +8,7 @@
  * Author URI:        https://eventkoi.com
  * License:           GPLv2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       eventkoi
+ * Text Domain:       eventkoi-lite
  * Domain Path:       /languages
  * Requires at least: 6.7
  * Requires PHP:      8.0
