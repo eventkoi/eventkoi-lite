d16bd328c15aa51e1c8ce1d0633a350c6723b0f0
ahmedfouaddev <ahmed@eventkoi.com>
2025-12-10 01:11:11 +0200

    Update build

--- scripts/frontend/build/index.asset.php
+++ scripts/frontend/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-primitives'), 'version' => 'c731809cceec86db2abb');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-primitives'), 'version' => 'f3744062b278fe5a7eec');
