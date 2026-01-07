aa53696356668a3ea36d7498575a55073c83102b
ahmedfouaddev <ahmedfouaddev@gmail.com>
2026-01-07 19:49:47 +0200

    Remove link

--- scripts/backend/src/components/dashboard/quick-links.js
+++ scripts/backend/src/components/dashboard/quick-links.js
@@ -2,16 +2,12 @@ import { BigLink } from "@/components/big-link";
 import { Box } from "@/components/box";
 import { Heading } from "@/components/heading";
 
-import { BookOpen, CircleUser, Inbox } from "lucide-react";
+import { BookOpen, Inbox } from "lucide-react";
 
 export function QuickLinks() {
   return (
     <Box container>
       <Heading level={3}>Quick links</Heading>
-      <BigLink href="https://pro.eventkoi.com/account">
-        <CircleUser className="w-5 h-5 mr-4" />
-        <span>Go to my account</span>
-      </BigLink>
       <BigLink href="https://eventkoi.com/docs">
         <BookOpen className="w-5 h-5 mr-4" />
         <span>Read documentation</span>
