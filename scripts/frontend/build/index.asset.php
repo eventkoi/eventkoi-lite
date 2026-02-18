1394fce2ccc60aa727f055507c947c8eda04d57e
ahmed <ahmedfouaddev@gmail.com>
2026-02-18 07:18:39 +0200

    Update builds

--- scripts/frontend/build/index.asset.php
+++ scripts/frontend/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => '3b40916560af5f823797');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => '354c5f494a4c2df3af19');
