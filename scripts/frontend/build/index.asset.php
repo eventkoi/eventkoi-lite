08b614f51b438c9ddf59c88319b164c0caca4925
EventKoi <dev@eventkoi.com>
2026-05-06 16:54:17 +0300

    Build assets for production

--- scripts/frontend/build/index.asset.php
+++ scripts/frontend/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => 'bc6b6be841924ed77f2d');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => '7e8b1b383444ecb1f769');
